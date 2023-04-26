7bccb20702b17f8ca3b4fc68ff80913f26a9608b
Cordinall <109527661+Cordinall@users.noreply.github.com>
2023-04-26 04:49:28 +0300

    Add files via upload

--- PYGame_DD/PYGame_DD.py
+++ PYGame_DD/PYGame_DD.py
@@ -2,19 +2,32 @@
 from Dangeon_Room import *
 import random
 
-def Rand_Room_shell():
-    a = random.randint(1,3)
-    b = random.randint(1,3)
+Inventory_bag = []
+Inventory_body = []
 
-    return a,b
+Current_difficuty = 0
 
-Inv = []
+Enemy_max_dmg = Current_difficuty + 2
+Enemy_min_dmg = Current_difficuty + 1
+Enemy_max_hp = Current_difficuty + 5
+Enemy_min_hp = Current_difficuty + 1
+Chest_treasure_max_dmg = Current_difficuty + 3
+Chest_treasure_min_dmg = Current_difficuty + 1
 
-Room_shell_1 = Dangeon_Room_NonAction(1,3)
-Room_shell_1.VisualGeneration()
-Room_chest_1 = Dangeon_Room_Action_Chest(1,1,1)
-Room_chest_1.VisualGeneraton()
+ExitDangeon = 0
+while ExitDangeon == 0:
 
-Inv.append(Room_chest_1.ItemGeneration())
 
-print(Inv)
+    Entity_Room_size = random.randint(1,3)
+    Entity_Room_biome = random.randint(1,3)
+    Entity_Room = Dangeon_Room_NonAction(Entity_Room_size, Entity_Room_biome)
+
+    Entity_Room.VisualGeneration()
+
+    doPlayer = input("========================================================\nВыберете действие: 1. Осмотреться 2. Вывести список объектов 3. Подойти к *объект*")
+
+    
+
+    Entity_Enemy_name = random.randint(1,1)
+    Entity_Enemy_dmg = random.randint(1,1)
+    Entity_Enemy_hp = random.randint(1,1)
